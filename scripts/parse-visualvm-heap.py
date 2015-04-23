4f2980184096731f6ff1e9bfed8fef0ce2adc126
Michael Zhou <zhoumotongxue008@gmail.com>
2015-04-23 18:19:14 -0400

    Add script to parse memory output from Tracer / Visualvm

---
+++ scripts/parse-visualvm-heap.py
@@ -0,0 +1,21 @@
+#!/bin/python
+import csv
+import locale
+import sys
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+file = open(sys.argv[1])
+file.readline()
+reader = csv.reader(file)
+count = 0
+maxHeap = 0
+sumHeap = 0
+for r in reader:
+  count += 1
+  used = locale.atoi(r[2])
+  sumHeap += used 
+  maxHeap = used if used > maxHeap else maxHeap
+
+print "Average heap utilization: " + str(sumHeap / count / 1000.0) + " MB"
+print "Max heap utilization: " + str(maxHeap / 1000.0) + " MB"
